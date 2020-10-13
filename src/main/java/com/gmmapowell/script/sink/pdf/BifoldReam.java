8fb7b6fdcce850ef84e3e0e43d0ba30a5a34e342
Gareth Powell <gareth@ziniki.org>
2020-10-13 09:31:53 -0400

    Added single and bifold reams

---
+++ src/main/java/com/gmmapowell/script/sink/pdf/BifoldReam.java
@@ -0,0 +1,59 @@
+package com.gmmapowell.script.sink.pdf;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.zinutils.exceptions.CantHappenException;
+
+import com.gmmapowell.script.styles.PageStyle;
+
+public class BifoldReam extends CommonReam implements Ream {
+	private final int blksize;
+	private final float swid;
+	private final float ht;
+	private final PDRectangle size;
+	private int cnt = 0;
+	private final List<PDPageContentStream> streams = new ArrayList<>();
+	
+	public BifoldReam(int blksize, float swid, float ht) {
+		if (blksize % 4 != 0)
+			throw new CantHappenException("bifold requires multiple of 4");
+		this.blksize = blksize;
+		this.swid = swid;
+		this.ht = ht;
+		size = new PDRectangle(swid * 2, ht);
+	}
+	
+	@Override
+	public PageCompositor newPage(PageStyle left, PageStyle right) throws IOException {
+		PDPageContentStream stream;
+		int mod;
+		do {
+			mod = cnt % blksize;
+			if (mod == 0) {
+				closeAllStreams();
+				streams.clear();
+			}
+			if (mod < blksize/2) { // add new pages
+				PDPage page = new PDPage(size);
+				doc.addPage(page);
+				stream = new PDPageContentStream(doc, page);
+				streams.add(stream);
+			} else {
+				stream = streams.get(blksize-mod-1);
+			}
+		} while (cnt++ % 2 == 1 && left == null);
+		SimplePageCompositor ret = new SimplePageCompositor(styles, stream, new PDRectangle((mod+1)%2*swid, 0, swid, ht), mod%2 == 1 ? left : right);
+		return ret;
+	}
+	
+	@Override
+	protected void closeAllStreams() throws IOException {
+		for (PDPageContentStream stream : streams)
+			stream.close();
+	}
+}
---
+++ src/main/java/com/gmmapowell/script/sink/pdf/SingleReam.java
@@ -0,0 +1,41 @@
+package com.gmmapowell.script.sink.pdf;
+
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+
+import com.gmmapowell.script.styles.PageStyle;
+
+public class SingleReam extends CommonReam implements Ream {
+	private int cnt = 0;
+	private final float wid;
+	private final float ht;
+	private final PDRectangle size;
+	private PDPageContentStream stream;
+	
+	public SingleReam(float wid, float ht) {
+		this.wid = wid;
+		this.ht = ht;
+		size = new PDRectangle(wid, ht);
+	}
+	
+	@Override
+	public PageCompositor newPage(PageStyle left, PageStyle right) throws IOException {
+		if (stream != null)
+			stream.close();
+		PDPage page = new PDPage(size);
+		doc.addPage(page);
+		stream = new PDPageContentStream(doc, page);
+		SimplePageCompositor ret = new SimplePageCompositor(styles, stream, new PDRectangle(0, 0, wid, ht), right);
+		cnt++;
+		return ret;
+	}
+	
+	@Override
+	protected void closeAllStreams() throws IOException {
+		if (stream != null)
+			stream.close();
+	}
+}
