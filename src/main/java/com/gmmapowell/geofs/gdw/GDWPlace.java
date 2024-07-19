609140b2cf3ebae6092a1445b5b6e0aa6e96af2c
Gareth Powell <gareth@ziniki.org>
2024-07-18 21:00:37 -0400

    Introducing line listener output stream

--- src/main/java/com/gmmapowell/geofs/gdw/GDWPlace.java
+++ src/main/java/com/gmmapowell/geofs/gdw/GDWPlace.java
@@ -1,22 +1,35 @@
 package com.gmmapowell.geofs.gdw;
 
+import java.io.IOException;
+
 import org.zinutils.exceptions.NotImplementedException;
 
 import com.gmmapowell.geofs.Place;
 import com.gmmapowell.geofs.Region;
+import com.gmmapowell.geofs.exceptions.GeoFSException;
 import com.gmmapowell.geofs.listeners.BinaryBlockListener;
 import com.gmmapowell.geofs.listeners.CharBlockListener;
 import com.gmmapowell.geofs.listeners.LineListener;
 import com.gmmapowell.geofs.listeners.NumberedLineListener;
+import com.gmmapowell.geofs.utils.LineListenerOutputStream;
 import com.google.api.services.drive.Drive;
 
 public class GDWPlace implements Place {
+	private final Drive service;
+	private final String id;
+
 	public GDWPlace(Drive service, String id) {
+		this.service = service;
+		this.id = id;
 	}
 
 	@Override
 	public void lines(LineListener lsnr) {
-		throw new NotImplementedException();
+		try {
+			service.files().export(id, "text/plain").executeMediaAndDownloadTo(LineListenerOutputStream.oslsnr(lsnr));
+		} catch (IOException ex) {
+			throw new GeoFSException(ex);
+		}
 	}
 
 	@Override
