8b139dd8ceb06bf7bb717c1f2d627e1f104e4e80
Gareth Powell <gareth@ziniki.org>
2024-08-01 10:19:11 -0400

    Finished LineListenerOutputStream impl

--- src/main/java/com/gmmapowell/geofs/utils/LineListenerOutputStream.java
+++ src/main/java/com/gmmapowell/geofs/utils/LineListenerOutputStream.java
@@ -9,44 +9,82 @@ import javax.swing.text.Segment;
 import org.apache.commons.io.output.WriterOutputStream;
 
 import com.gmmapowell.geofs.listeners.LineListener;
+import com.gmmapowell.geofs.listeners.NumberedLineListener;
 
 public class LineListenerOutputStream extends Writer {
 	private final LineListener lsnr;
+	private final NumberedLineListener nlsnr;
+	private final StringBuilder buffer = new StringBuilder();
+	private int lno = 0;
 
 	public static OutputStream oslsnr(LineListener lsnr) {
+		WriterOutputStream wos = new WriterOutputStream(new LineListenerOutputStream(lsnr), "UTF-8");
+		return wos;
+	}
+
+	public static OutputStream oslsnr(NumberedLineListener lsnr) {
 		WriterOutputStream wos = new WriterOutputStream(new LineListenerOutputStream(lsnr), "UTF-8");
 		return wos;
 	}
 	
 	public LineListenerOutputStream(LineListener lsnr) {
 		this.lsnr = lsnr;
+		this.nlsnr = null;
 	}
 
+	public LineListenerOutputStream(NumberedLineListener lsnr) {
+		this.nlsnr = lsnr;
+		this.lsnr = null;
+	}
 
 	@Override
 	public void write(char[] cbuf, int off, int len) throws IOException {
 		Segment sg = new Segment(cbuf, off, len);
 		int from = 0;
 		for (int i=0;i<sg.length();i++) {
-			System.out.println("char " + i + " of " + sg + " => " + sg.charAt(i));
+//			System.out.println("char " + i + " of " + sg + " => " + sg.charAt(i));
 			if (sg.charAt(i) == '\n') {
-				System.out.println("have " + sg.subSequence(from, i));
-				lsnr.line(sg.subSequence(from, i).toString());
+				int to = i;
+				if (to > 0 && sg.charAt(to-1) == '\r')
+					to--;
+//				System.out.println("have " + sg.subSequence(from, to));
+				if (buffer.length() > 0) {
+					buffer.append(sg.subSequence(from, to));
+					line(buffer.toString());
+					buffer.setLength(0);
+				} else {
+					line(sg.subSequence(from, to).toString());
+				}
 				from = i+1;
 			}
 		}
 		if (from < sg.length())
-			lsnr.line(sg.subSequence(from, sg.length()).toString());
+			buffer.append(sg.subSequence(from, sg.length()).toString());
 	}
 
 	@Override
 	public void flush() throws IOException {
-		// TODO Auto-generated method stub
-		
+		// because we do all the work directly in write(), flush() does not do anything
+		// we flush unfinished lines only in complete
 	}
 
 	@Override
 	public void close() throws IOException {
-		lsnr.complete();
+		if (buffer.length() > 0) {
+			line(buffer.toString());
+		}
+		if (lsnr != null) {
+			lsnr.complete();
+		} else {
+			nlsnr.complete();
+		}
+	}
+
+	private void line(String line) {
+		if (lsnr != null) {
+			lsnr.line(line);
+		} else {
+			nlsnr.line(++lno, line);
+		}
 	}
 }
--- src/test/java/test/geofs/utils/LineListenerOutputStreamTests.java
+++ src/test/java/test/geofs/utils/LineListenerOutputStreamTests.java
@@ -9,8 +9,11 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import com.gmmapowell.geofs.listeners.LineListener;
+import com.gmmapowell.geofs.listeners.NumberedLineListener;
 import com.gmmapowell.geofs.utils.LineListenerOutputStream;
 
+// TODO: there is something to be said for introducing a "Sequence" to ensure that everything comes through in the right order
+// But I think that is logically going to happen.
 public class LineListenerOutputStreamTests {
 	@Rule public JUnitRuleMockery context = new JUnitRuleMockery();
 
@@ -39,6 +42,21 @@ public class LineListenerOutputStreamTests {
 	}
 
 	@Test
+	public void twoPartialLinesComesAreJoined() throws IOException {
+		LineListener lsnr = context.mock(LineListener.class);
+		OutputStream os = LineListenerOutputStream.oslsnr(lsnr);
+		os.write("hello, ".getBytes());
+		os.flush();
+		os.write("world".getBytes());
+		os.flush();
+		context.checking(new Expectations() {{
+			oneOf(lsnr).line("hello, world");
+			oneOf(lsnr).complete();
+		}});
+		os.close();
+	}
+
+	@Test
 	public void twoLinesComeThroughSeparately() throws IOException {
 		LineListener lsnr = context.mock(LineListener.class);
 		context.checking(new Expectations() {{
@@ -48,6 +66,62 @@ public class LineListenerOutputStreamTests {
 		}});
 		OutputStream os = LineListenerOutputStream.oslsnr(lsnr);
 		os.write("hello, world\ngood morning\n".getBytes());
+		os.close();
+	}
+
+	@Test
+	public void newlinesInTheMiddleOfTwoStringsAreResolvedCorrectly() throws IOException {
+		LineListener lsnr = context.mock(LineListener.class);
+		context.checking(new Expectations() {{
+			oneOf(lsnr).line("good morning");
+			oneOf(lsnr).line("hello, world");
+			oneOf(lsnr).line("Salutations!");
+			oneOf(lsnr).complete();
+		}});
+		OutputStream os = LineListenerOutputStream.oslsnr(lsnr);
+		os.write("good morning\nhello, ".getBytes());
+		os.flush();
+		os.write("world\nSalutations!".getBytes());
+		os.flush();
+		os.close();
+	}
+
+	@Test
+	public void newlineBecomesABlankLine() throws IOException {
+		LineListener lsnr = context.mock(LineListener.class);
+		context.checking(new Expectations() {{
+			oneOf(lsnr).line("hello,");
+			oneOf(lsnr).line("");
+			oneOf(lsnr).line("world");
+			oneOf(lsnr).complete();
+		}});
+		OutputStream os = LineListenerOutputStream.oslsnr(lsnr);
+		os.write("hello,\n\nworld".getBytes());
+		os.close();
+	}
+
+	@Test
+	public void initialNewlineBecomesABlankLine() throws IOException {
+		LineListener lsnr = context.mock(LineListener.class);
+		context.checking(new Expectations() {{
+			oneOf(lsnr).line("");
+			oneOf(lsnr).line("hello, world");
+			oneOf(lsnr).complete();
+		}});
+		OutputStream os = LineListenerOutputStream.oslsnr(lsnr);
+		os.write("\nhello, world\n".getBytes());
+		os.close();
+	}
+
+	@Test
+	public void crBeforeNlIsTrimmed() throws IOException {
+		LineListener lsnr = context.mock(LineListener.class);
+		context.checking(new Expectations() {{
+			oneOf(lsnr).line("hello, world");
+			oneOf(lsnr).complete();
+		}});
+		OutputStream os = LineListenerOutputStream.oslsnr(lsnr);
+		os.write("hello, world\r\n".getBytes());
 		os.close();
 	}
 
@@ -62,10 +136,22 @@ public class LineListenerOutputStreamTests {
 		os.write(" hello, world \n".getBytes());
 		os.close();
 	}
-	// TODO: \n at the front
-	// TODO: \r\n
-	// TODO: \n\n
-	// TODO: split across two writes
-	// TODO: we can have line numbers as well
+
+	@Test
+	public void lineNumbersAreAvailable() throws IOException {
+		NumberedLineListener lsnr = context.mock(NumberedLineListener.class);
+		context.checking(new Expectations() {{
+			oneOf(lsnr).line(1, "good morning");
+			oneOf(lsnr).line(2, "hello, world");
+			oneOf(lsnr).line(3, "Salutations!");
+			oneOf(lsnr).complete();
+		}});
+		OutputStream os = LineListenerOutputStream.oslsnr(lsnr);
+		os.write("good morning\nhello, ".getBytes());
+		os.flush();
+		os.write("world\nSalutations!".getBytes());
+		os.flush();
+		os.close();
+	}
 }
 
