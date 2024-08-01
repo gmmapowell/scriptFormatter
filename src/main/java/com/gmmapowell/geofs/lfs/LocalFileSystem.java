dc2a648a0c3208adfc52218fb553710983a20f41
Gareth Powell <gareth@ziniki.org>
2024-08-01 09:57:25 -0400

    Added ~ as a root

--- src/main/java/com/gmmapowell/geofs/lfs/LocalFileSystem.java
+++ src/main/java/com/gmmapowell/geofs/lfs/LocalFileSystem.java
@@ -2,13 +2,13 @@ package com.gmmapowell.geofs.lfs;
 
 import java.io.File;
 
-import org.zinutils.exceptions.CantHappenException;
 import org.zinutils.exceptions.NotImplementedException;
 
 import com.gmmapowell.geofs.Place;
 import com.gmmapowell.geofs.Region;
 import com.gmmapowell.geofs.Universe;
 import com.gmmapowell.geofs.World;
+import com.gmmapowell.geofs.utils.GeoFSUtils;
 
 public class LocalFileSystem implements World {
 	private Universe universe;
@@ -31,26 +31,19 @@ public class LocalFileSystem implements World {
 
 	@Override
 	public Region root(String root) {
-		throw new NotImplementedException();
+		if (root.equals("~")) {
+			return new LFSRegion(this, new File(System.getProperty("user.home")));
+		} else
+			throw new NotImplementedException();
 	}
 
 	@Override
 	public Region regionPath(String path) {
-		File f = new File(path);
-		if (!f.isAbsolute())
-			throw new CantHappenException("relative paths cannot be used from the root");
-		if (!f.isDirectory())
-			throw new CantHappenException("there is no region " + f);
-		return new LFSRegion(this, f);
+		return GeoFSUtils.regionPath(this, null, path);
 	}
 
 	@Override
 	public Place placePath(String path) {
-		File f = new File(path);
-		if (!f.isAbsolute())
-			throw new CantHappenException("relative paths must be used from a region");
-		if (!f.isFile())
-			throw new CantHappenException("there is no place " + f);
-		return new LFSPlace(this, f);
+		return GeoFSUtils.placePath(this, null, path);
 	}
 }
