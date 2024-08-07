bb8f4659553b6839a4cb9fe297d25eeb4b87b770
Gareth Powell <gareth@ziniki.org>
2024-08-07 17:38:04 -0400

    Improved citation parsing

--- src/test/java/test/emailquoter/CitationParsing.java
+++ src/test/java/test/emailquoter/CitationParsing.java
@@ -9,11 +9,42 @@ import com.gmmapowell.script.modules.emailquoter.Citation;
 public class CitationParsing {
 
 	@Test
-	public void test() {
+	public void aSingleNumberSetsFirstAndLast() {
 		Citation c = Citation.parse("file", "1");
 		assertEquals("file", c.getFile());
 		assertEquals(1, c.getFirst());
+		assertNull(c.getFromPhrase());
 		assertEquals(1, c.getLast());
+		assertNull(c.getToPhrase());
 	}
 
+	@Test
+	public void twoNumbersSeparatedByADashSetFirstAndLast() {
+		Citation c = Citation.parse("file", "1-3");
+		assertEquals("file", c.getFile());
+		assertEquals(1, c.getFirst());
+		assertNull(c.getFromPhrase());
+		assertEquals(3, c.getLast());
+		assertNull(c.getToPhrase());
+	}
+
+	@Test
+	public void aColonIntroducesAKeyPhrase() {
+		Citation c = Citation.parse("file", "1:Quickly, there-3");
+		assertEquals("file", c.getFile());
+		assertEquals(1, c.getFirst());
+		assertEquals("Quickly, there", c.getFromPhrase());
+		assertEquals(3, c.getLast());
+		assertNull(c.getToPhrase());
+	}
+
+	@Test
+	public void aColonIntroducesAToPhraseAsWell() {
+		Citation c = Citation.parse("file", "1:Quickly, there-3:hello.");
+		assertEquals("file", c.getFile());
+		assertEquals(1, c.getFirst());
+		assertEquals("Quickly, there", c.getFromPhrase());
+		assertEquals(3, c.getLast());
+		assertEquals("hello.", c.getToPhrase());
+	}
 }
--- src/test/java/test/emailquoter/CommandArgParsingTest.java
+++ src/test/java/test/emailquoter/CommandArgParsingTest.java
@@ -10,11 +10,43 @@ import com.gmmapowell.script.processor.prose.SBLineArgsParser;
 public class CommandArgParsingTest {
 
 	@Test
-	public void test() {
+	public void aNumberIsEasyToParse() {
 		DocState state = null;
 		SBLineArgsParser<DocState> p = new SBLineArgsParser<>(state, "15");
 		assertEquals("15", p.readArg());
 		p.argsDone();
 	}
 
+	@Test
+	public void aColonIsCountedAsTheSameArg() {
+		DocState state = null;
+		SBLineArgsParser<DocState> p = new SBLineArgsParser<>(state, "15:xy");
+		assertEquals("15:xy", p.readArg());
+		p.argsDone();
+	}
+
+	@Test
+	public void aSpaceEndsAnArg() {
+		DocState state = null;
+		SBLineArgsParser<DocState> p = new SBLineArgsParser<>(state, "15 xy");
+		assertEquals("15", p.readArg());
+		assertEquals("xy", p.readArg());
+		p.argsDone();
+	}
+
+	@Test
+	public void aSpaceCanBeQuoted() {
+		DocState state = null;
+		SBLineArgsParser<DocState> p = new SBLineArgsParser<>(state, "'15 xy'");
+		assertEquals("15 xy", p.readArg());
+		p.argsDone();
+	}
+
+	@Test
+	public void aSpaceCanBeDoubleQuoted() {
+		DocState state = null;
+		SBLineArgsParser<DocState> p = new SBLineArgsParser<>(state, "\"15 xy\"");
+		assertEquals("15 xy", p.readArg());
+		p.argsDone();
+	}
 }
