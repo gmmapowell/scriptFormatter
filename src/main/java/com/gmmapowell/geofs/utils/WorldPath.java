190473aa77e9d6cb55f5ea5e88d641c0dd030256
Gareth Powell <gareth@ziniki.org>
2024-09-08 09:37:30 +0100

    Extracted root properly

--- src/main/java/com/gmmapowell/geofs/utils/WorldPath.java
+++ src/main/java/com/gmmapowell/geofs/utils/WorldPath.java
@@ -13,15 +13,24 @@ import com.gmmapowell.geofs.exceptions.GeoFSInvalidWorldException;
 public class WorldPath {
 	private static Pattern uriStyle = Pattern.compile("([a-z0-9]+)://(.*)");
 	private static Pattern uriStyleWithRoot = Pattern.compile("([a-z0-9]+)://(.*):([^:]*)");
+
 	World inWorld;
+	String root;
 	String path;
 	File f;
 
 	public static WorldPath parse(World world, String path) {
 		Matcher isUri = uriStyle.matcher(path);
 		World other = world;
+		String root = null;
 		if (isUri.matches()) {
-			path = isUri.group(2);
+			Matcher multiParts = uriStyleWithRoot.matcher(path);
+			if (multiParts.matches()) {
+				root = multiParts.group(2);
+				path = multiParts.group(3);
+			} else {
+				path = isUri.group(2);
+			}
 			Universe u = world.getUniverse();
 			if (u == null) {
 				throw new CantHappenException("the World is not part of a Universe");
@@ -29,11 +38,12 @@ public class WorldPath {
 			other = u.getWorld(isUri.group(1));
 		}
 		File f = new File(path);
-		return new WorldPath(other, path, f);
+		return new WorldPath(other, root, path, f);
 	}
 
-	private WorldPath(World other, String path, File f) {
+	private WorldPath(World other, String root, String path, File f) {
 		this.inWorld = other;
+		this.root = root;
 		this.path = path;
 		this.f = f;
 	}
