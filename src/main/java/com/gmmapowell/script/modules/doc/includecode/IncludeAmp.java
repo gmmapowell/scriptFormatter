520da8e81a7b053826ed6cea49ec9c3d95279ce2
Gareth Powell <gareth@ziniki.org>
2024-09-10 10:08:43 +0100

    Updating include with havePlace

--- src/main/java/com/gmmapowell/script/modules/doc/includecode/IncludeAmp.java
+++ src/main/java/com/gmmapowell/script/modules/doc/includecode/IncludeAmp.java
@@ -1,5 +1,6 @@
 package com.gmmapowell.script.modules.doc.includecode;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
@@ -8,6 +9,7 @@ import org.zinutils.exceptions.WrappedException;
 
 import com.gmmapowell.geofs.Place;
 import com.gmmapowell.geofs.Region;
+import com.gmmapowell.geofs.utils.GeoFSUtils;
 import com.gmmapowell.script.modules.processors.doc.AmpCommand;
 import com.gmmapowell.script.modules.processors.doc.AmpCommandHandler;
 import com.gmmapowell.script.modules.processors.doc.GlobalState;
@@ -53,10 +55,16 @@ public class IncludeAmp implements AmpCommandHandler {
 		Region samples = ic.samples();
 		if (samples == null)
 			throw new CantHappenException("samples region has not been specified");
+		Region r = samples;
 		String file = cmd.args.readString();
 		Map<String, String> params = cmd.args.readParams("formatter");
 		System.out.println("want to include " + file + " with " + params);
-		if (!samples.hasPlace(file)) {
+		if (file.indexOf('/') != -1) {
+			File f = new File(file);
+			r = GeoFSUtils.regionPath(null, samples, f.getParent());
+			file = f.getName();
+		}
+		if (!r.hasPlace(file)) {
 			throw new RuntimeException("there is no sample " + file + " in " + samples + " at " + state.inputLocation());
 		}
 		Place code = samples.place(file);
